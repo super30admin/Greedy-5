b938df42d5ab3d20677de6deafbef506f30aa5d6
meghasn <41339621+meghasn@users.noreply.github.com>
2021-12-06 17:04:01 +0530

    added broken calculator

---
+++ broken_calc_greedy.py
@@ -0,0 +1,22 @@
+# // Time Complexity :O(logn)
+# // Space Complexity :O(1)
+# // Did this code successfully run on Leetcode :yes
+# // Any problem you faced while coding this :no
+
+#greedy, give one path of bottom up order
+#o(logn)
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        count=0
+        if startValue==target:
+            return count
+        while target>startValue:
+            print(target)
+            if target%2==0:
+                target/=2
+            else:
+                target+=1
+            count+=1
+        return int(count+startValue-target)
+                
+        
